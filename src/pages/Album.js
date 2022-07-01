2a012f8c4973ccf0e3b0a97d56a4051dc819c1c7
heloisasantana <heloisasantana.silva@hotmail.com>
2022-07-01 16:40:43 -0300

    Conclusão do Requisito 1

---
+++ src/pages/Album.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Album extends React.Component {
+  render() {
+    return (
+      <div data-testid="page-album">
+        Oi
+      </div>
+    );
+  }
+}
+
+export default Album;
