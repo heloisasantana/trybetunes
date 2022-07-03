cf8e15a2ecbb43fccee486df33d630dba6ac211b
heloisasantana <heloisasantana.silva@hotmail.com>
2022-07-03 00:06:50 -0300

    Conclusão do Requisito 5

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -2,10 +2,46 @@ import React from 'react';
 import Header from '../components/Header';
 
 class Search extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      isDisabled: true,
+    };
+    this.validateSearch = this.validateSearch.bind(this);
+  }
+
+  validateSearch(event) {
+    const { length } = event.target.value;
+    const min = Number('2');
+    if (length >= min) {
+      this.setState({
+        isDisabled: false,
+      });
+    }
+  }
+
   render() {
+    const { isDisabled } = this.state;
     return (
       <div data-testid="page-search">
         <Header />
+        <form>
+          <label htmlFor="search-artist-input">
+            <input
+              type="text"
+              data-testid="search-artist-input"
+              onChange={ this.validateSearch }
+            />
+          </label>
+          <label htmlFor="search-artist-button">
+            <input
+              type="button"
+              data-testid="search-artist-button"
+              value="Pesquisar"
+              disabled={ isDisabled }
+            />
+          </label>
+        </form>
       </div>
     );
   }
