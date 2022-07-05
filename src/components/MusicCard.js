e8a9adc0d8be2378c146a52b24093eb4966cf594
heloisasantana <heloisasantana.silva@hotmail.com>
2022-07-05 17:33:36 -0300

    Conclusão do Requisito 9

--- src/components/MusicCard.js
+++ src/components/MusicCard.js
@@ -1,54 +1,27 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { addSong } from '../services/favoriteSongsAPI';
-import Loading from './Loading';
 
 class MusicCard extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      loading: false,
-      isFavorited: false,
-    };
-    this.handleFavorite = this.handleFavorite.bind(this);
-  }
-
-  handleFavorite() {
-    this.setState({
-      loading: true,
-    }, async () => {
-      await addSong();
-      this.setState({
-        loading: false,
-        isFavorited: true,
-      });
-    });
-  }
-
   render() {
-    const { loading, isFavorited } = this.state;
-    const { trackName, trackId, previewUrl } = this.props;
+    const { trackName, trackId, previewUrl, isFavorited, onCheckboxChange } = this.props;
     return (
       <div>
-        { loading ? (<Loading />) : (
-          <section>
-            <h5>{ trackName }</h5>
-            <label htmlFor={ `checkbox-music-${trackId}` }>
-              Favoritar
-              <input
-                type="checkbox"
-                data-testid={ `checkbox-music-${trackId}` }
-                checked={ isFavorited }
-                onClick={ this.handleFavorite }
-              />
-            </label>
-            <audio data-testid="audio-component" src={ previewUrl } controls>
-              <track kind="captions" />
-              O seu navegador não suporta o elemento
-              <code>audio</code>
-              .
-            </audio>
-          </section>)}
+        <h5>{ trackName }</h5>
+        <label htmlFor={ `checkbox-music-${trackId}` }>
+          Favoritar
+          <input
+            type="checkbox"
+            data-testid={ `checkbox-music-${trackId}` }
+            defaultChecked={ isFavorited }
+            onChange={ onCheckboxChange }
+          />
+        </label>
+        <audio data-testid="audio-component" src={ previewUrl } controls>
+          <track kind="captions" />
+          O seu navegador não suporta o elemento
+          <code>audio</code>
+          .
+        </audio>
       </div>
     );
   }
@@ -58,6 +31,8 @@ MusicCard.propTypes = {
   trackName: PropTypes.string.isRequired,
   trackId: PropTypes.number.isRequired,
   previewUrl: PropTypes.string.isRequired,
+  isFavorited: PropTypes.bool.isRequired,
+  onCheckboxChange: PropTypes.func.isRequired,
 };
 
 export default MusicCard;
--- src/pages/Album.js
+++ src/pages/Album.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import getMusics from '../services/musicsAPI';
 import Header from '../components/Header';
+import Loading from '../components/Loading';
 import MusicCard from '../components/MusicCard';
+import getMusics from '../services/musicsAPI';
+import { addSong, getFavoriteSongs } from '../services/favoriteSongsAPI';
 
 class Album extends React.Component {
   constructor() {
@@ -11,46 +13,83 @@ class Album extends React.Component {
       album: '',
       artistOrBand: '',
       musicList: [],
+      favoriteList: [],
+      loading: false,
     };
+    this.handleCheckbox = this.handleCheckbox.bind(this);
+    this.verificationFavorite = this.verificationFavorite.bind(this);
   }
 
   componentDidMount() {
     // Referência para realizar o Destructuring Assignment e consertar o erro do Lint: https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Operators/Destructuring_assignment;
     const { match: { params: { id } } } = this.props;
-    const recoveringMusics = async () => {
-      const data = await getMusics(id);
-      const { collectionName } = data[0];
-      const { artistName } = data[0];
+    this.setState({ loading: true }, async () => {
+      const dataMusics = await getMusics(id);
       // Referência para utilizar o slice para fazer uma cópia de um array a partir de um ponto específico: https://www.w3schools.com/jsref/jsref_slice_array.asp;
-      const musics = data.slice(1);
-      this.setState({
+      const { collectionName } = dataMusics[0];
+      const { artistName } = dataMusics[0];
+      const onlyMusics = dataMusics.slice(1);
+      const dataFavorites = await getFavoriteSongs();
+      this.setState((prev) => ({
         album: collectionName,
         artistOrBand: artistName,
-        musicList: musics,
-      });
-    };
-    recoveringMusics();
+        musicList: onlyMusics,
+        favoriteList: [...prev.favoriteList, ...dataFavorites],
+        loading: false,
+      }));
+    });
+  }
+
+  handleCheckbox(music) {
+    this.setState({
+      loading: true,
+    }, async () => {
+      await addSong(music);
+      const uptadeFavorites = await getFavoriteSongs();
+      const lastPosition = uptadeFavorites.length - 1;
+      const lastUptade = uptadeFavorites[lastPosition];
+      this.setState((prev) => ({
+        favoriteList: [...prev.favoriteList, lastUptade],
+        loading: false,
+      }));
+    });
+  }
+
+  verificationFavorite(trackId) {
+    const { favoriteList } = this.state;
+    let result = false;
+    const verify = favoriteList
+      .filter((favoriteItem) => favoriteItem.trackId === trackId);
+    if (verify.length > 0) { result = true; }
+    if (verify.length === 0) { result = false; }
+    return result;
   }
 
   render() {
-    const { album, artistOrBand, musicList } = this.state;
+    const { album, artistOrBand, musicList, loading } = this.state;
     return (
       <div data-testid="page-album">
         <Header />
-        <h4 data-testid="album-name">
-          Album:
-          {` ${album}`}
-        </h4>
-        <h4 data-testid="artist-name">
-          {`${artistOrBand}`}
-        </h4>
-        { musicList.map(({ trackId, trackName, previewUrl }) => (
-          <MusicCard
-            key={ trackId }
-            trackName={ trackName }
-            trackId={ trackId }
-            previewUrl={ previewUrl }
-          />))}
+        { loading ? (<Loading />) : (
+          <div>
+            <h4 data-testid="album-name">
+              Album:
+              {` ${album}`}
+            </h4>
+            <h4 data-testid="artist-name">
+              {`${artistOrBand}`}
+            </h4>
+            { musicList.map((music) => (
+              <MusicCard
+                key={ music.trackId }
+                trackName={ music.trackName }
+                trackId={ music.trackId }
+                previewUrl={ music.previewUrl }
+                onCheckboxChange={ () => this.handleCheckbox(music) }
+                isFavorited={ this.verificationFavorite(music.trackId) }
+              />))}
+          </div>
+        )}
       </div>
     );
   }
